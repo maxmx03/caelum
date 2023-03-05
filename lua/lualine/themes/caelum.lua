3e620501006e3f489ac9ae80b4b9e511a2d736ea
Max Miliano <milianordelcanto@gmail.com>
2023-03-05 18:54:27 -0300

    update

---
+++ lua/lualine/themes/caelum.lua
@@ -0,0 +1,37 @@
+local colors = {
+  background = '#1F1F1F',
+  background2 = '#2F2F2F',
+  foreground = '#CABEAC',
+  foreground2 = '#a09483',
+  green = '#7B8F5A',
+  yellow = '#DCC68D',
+  blue = '#677FA3',
+  gray = '#848e8A',
+  red = '#de6464',
+  orange = '#E58538',
+  purple = '#7B77A0',
+  magenta = '#D885A3',
+}
+
+local caelum = {
+  normal = {
+    a = { fg = colors.blue, bg = colors.background, gui = 'bold' },
+    b = { fg = colors.foreground2, bg = colors.background2 },
+    c = { fg = colors.foreground2, bg = colors.background2 },
+    z = { fg = colors.blue, bg = colors.background },
+  },
+  insert = {
+    a = { fg = colors.green, bg = colors.background },
+  },
+  visual = {
+    a = { fg = colors.purple, bg = colors.backgrund },
+  },
+  replace = {
+    a = { fg = colors.orange, bg = colors.background },
+  },
+  command = {
+    a = { fg = colors.backgrund, bg = colors.blue },
+  },
+}
+
+return caelum
