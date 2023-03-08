de1e9ec3f46c00a73022175e3f989b173e7a1c4d
Max Miliano <milianordelcanto@gmail.com>
2023-03-07 21:54:09 -0300

    update lualine

--- lua/lualine/themes/caelum.lua
+++ lua/lualine/themes/caelum.lua
@@ -1,35 +1,38 @@
 local colors = {
-  background = '#1F1F1F',
-  foreground = '#CABEAC',
-  green = '#7B8F5A',
-  yellow = '#DCC68D',
-  blue = '#677FA3',
-  gray = '#848e8A',
-  red = '#de6464',
-  orange = '#E58538',
-  purple = '#7B77A0',
-  magenta = '#D885A3',
+	background = "#1F1F1F",
+  background_alt = '#3F3F3F',
+	foreground = "#CABEAC",
+	green = "#7B8F5A",
+	yellow = "#DCC68D",
+	blue = "#677FA3",
+	red = "#de6464",
+	orange = "#E58538",
+	purple = "#7B77A0",
+	magenta = "#D885A3",
 }
 
 local caelum = {
-  normal = {
-    a = { fg = colors.blue, bg = colors.background, gui = 'bold' },
-    b = { fg = colors.foreground, bg = colors.background },
-    c = { fg = colors.foreground, bg = colors.background },
-    z = { fg = colors.blue, bg = colors.background },
-  },
-  insert = {
-    a = { fg = colors.green, bg = colors.background },
-  },
-  visual = {
-    a = { fg = colors.purple, bg = colors.background },
-  },
-  replace = {
-    a = { fg = colors.orange, bg = colors.background },
-  },
-  command = {
-    a = { fg = colors.yellow, bg = colors.background },
-  },
+	normal = {
+		a = { bg = colors.background, fg = colors.blue, gui = "bold" },
+		b = { fg = colors.background, bg = colors.blue },
+		c = { fg = colors.fg, bg = colors.background },
+	},
+	insert = {
+		a = { bg = colors.background, fg = colors.green },
+		b = { fg = colors.background, bg = colors.green },
+	},
+	visual = {
+		a = { bg = colors.background, fg = colors.purple },
+		b = { fg = colors.background, bg = colors.purple },
+	},
+	replace = {
+		a = { bg = colors.background, fg = colors.orange },
+		b = { fg = colors.background, bg = colors.orange },
+	},
+	command = {
+		a = { bg = colors.background, fg = colors.yellow },
+		b = { fg = colors.background, bg = colors.yellow },
+	},
 }
 
 return caelum
